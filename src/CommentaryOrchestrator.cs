a140b5a6efbb74b9b5e4e7754d03569589151e6f
Divakar Kumar <divakar.kumar@flyerssoft.com>
2023-09-02 01:40:08 +0530

    updated code for orchestrator

--- src/CommentaryOrchestrator.cs
+++ src/CommentaryOrchestrator.cs
@@ -11,7 +11,7 @@ namespace Hotstar.Autoscaler
     {
         [FunctionName("Commentary_SchedulerJob")]
         public static async Task CommentarySchedulerJob(
-            [TimerTrigger("0 45 00 * * *")] TimerInfo timer,
+            [TimerTrigger("0 38 01 * * *")] TimerInfo timer,
             [DurableClient] IDurableOrchestrationClient starter,
             ILogger log)
         {
@@ -41,37 +41,48 @@ namespace Hotstar.Autoscaler
         [OrchestrationTrigger] IDurableOrchestrationContext context,
         ILogger log)
         {
-            List<string> commentaryList = GetCommentaryList();
-
-            int currentIndex = 0;
-
-            while (currentIndex < commentaryList.Count)
+            List<MatchCommentary> commentaryList = GetCommentaryList();
+            
+            foreach(var commentary in commentaryList) 
             {                
-                await context.CallActivityAsync("PublishToServiceBusQueue", commentaryList[currentIndex]);                
-                await context.CreateTimer(context.CurrentUtcDateTime.AddSeconds(20), CancellationToken.None);
-
-                currentIndex++;
+                await context.CallActivityAsync("PublishToServiceBusQueue", commentary);                
+                await context.CreateTimer(context.CurrentUtcDateTime.AddSeconds(20), CancellationToken.None);               
             }
         }
 
         [FunctionName("PublishToServiceBusQueue")]
         public static async Task PublishToServiceBusQueue(
-           [ActivityTrigger] string commentary,
-           [ServiceBus("%ServiceBusQueue%", Connection = "ServiceBusConnection")] IAsyncCollector<string> messageQueue,
+           [ActivityTrigger] MatchCommentary commentary,
+           [ServiceBus("%ServiceBusQueue%", Connection = "ServiceBusConnection")] IAsyncCollector<MatchCommentary> messageQueue,
            ILogger log)
         {
             await messageQueue.AddAsync(commentary);
             log.LogInformation($"Published to Queue: {commentary}");
         }
 
-        private static List<string> GetCommentaryList()
+        private static List<MatchCommentary> GetCommentaryList()
         {
-            return new List<string>
+            return new List<MatchCommentary>
         {
-            "Boult to Jadeja, out Caught by Williamson!! Massive wicket! NZ needed a wicket and his main strike bowler has done precisely that. Slower ball and for once, Jadeja didn't quite pick it as he went across the line. A bit too early and skewed it straight up in the air. Never easy, these pressure catches but it's the ice-cool KW at mid-off who settles under it calmly. Exceptional knock from Jadeja but he has to go. NZ in the driver's seat. Jadeja c Williamson b Boult 77(59) [4s-4 6s-4]",
-            "Ferguson to Chahal, no run, very full and outside off, squeezed out towards mid-off",
-            "Ferguson to Chahal, 1 run, full and angling into Chahal who flicks it away towards deep square leg"
+            new MatchCommentary{
+            matchId ="1",
+            commentary= "Boult to Jadeja, out Caught by Williamson!! Massive wicket! NZ needed a wicket and his main strike bowler has done precisely that. Slower ball and for once, Jadeja didn't quite pick it as he went across the line. A bit too early and skewed it straight up in the air. Never easy, these pressure catches but it's the ice-cool KW at mid-off who settles under it calmly. Exceptional knock from Jadeja but he has to go. NZ in the driver's seat. Jadeja c Williamson b Boult 77(59) [4s-4 6s-4]"
+            },
+            new MatchCommentary{
+                matchId ="1",
+                commentary="Ferguson to Chahal, no run, very full and outside off, squeezed out towards mid-off"
+            },
+            new MatchCommentary{
+                matchId="1",
+                commentary="Ferguson to Chahal, 1 run, full and angling into Chahal who flicks it away towards deep square leg"    
+            }
         };
+        }
+
+        public class MatchCommentary
+        {
+            public string commentary { get; set; }
+            public string matchId { get; set; }
         }
     }
 
