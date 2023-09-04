1c6df827895516e9cb441d44fcf38c96cbe8b31b
Divakar Kumar <divakar.kumar@flyerssoft.com>
2023-09-04 10:38:58 +0530

    added commentary

--- src/CommentaryOrchestrator.cs
+++ src/CommentaryOrchestrator.cs
@@ -11,7 +11,7 @@ namespace Hotstar.Autoscaler
     {
         [FunctionName("Commentary_SchedulerJob")]
         public static async Task CommentarySchedulerJob(
-            [TimerTrigger("0 57 01 * * *")] TimerInfo timer,
+            [TimerTrigger("0 59 23 * * *")] TimerInfo timer,
             [DurableClient] IDurableOrchestrationClient starter,
             ILogger log)
         {
@@ -74,8 +74,12 @@ namespace Hotstar.Autoscaler
             },
             new MatchCommentary{
                 matchId="1",
-                commentary="Ferguson to Chahal, 1 run, full and angling into Chahal who flicks it away towards deep square leg"    
-            }
+                commentary="Ferguson to Dhoni, out Dhoni Run Out!! 1 run completed. Guptill you genius of a fielder! Got to the ball quickly and fired in a terrific direct hit at the keeper's end. There was a slight stutter on the second run and cost Dhoni as he was just short. Ironically, for a man who is so good between the wickets, he had to fall to a run out. Guptill has had a bad tourney with the bat but hasn't he redeemed himself in a big way with this effort? Was a slower ball on a length and it popped up off the glove towards short fine leg as Dhoni got cramped on the pull. They took on Guppy for the second run and it cost MS. Dhoni run out (Guptill) 50(72) [4s-1 6s-1]"
+            },
+             new MatchCommentary{
+                matchId="1",
+                commentary="Boult to Jadeja, FOUR, the edge goes in the gap! It's turning folks, this game is certainly turning. When moments like these are going in the batting side's favor, momentum is bound to be with them. Was a shortish length ball that Jadeja wanted to guide with an open face, got it off the edge but he won't complain as it bisects the keeper and short third man perfectly"
+            }        
         };
         }
 
